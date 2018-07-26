88aa040524bdecd32a0bfb070f3c4fbae9700972
Yusuf Dwi Santoso <yusufdwisantoso@gmail.com>
2018-07-26 12:32:41 +0700

    tambahan dikit profil

--- resources/views/app/wna.blade.php
+++ resources/views/app/wna.blade.php
@@ -14,28 +14,114 @@
                         <li><a href="#wna2" data-toggle="tab">Izin Tinggal Terbatas</a></li>
                         <li><a href="#wna3" data-toggle="tab">Izin Tinggal Tetap</a></li>
                         <li><a href="#wna4" data-toggle="tab">Fasilitas Keimigrasian</a></li>
-                        <li><a href="#wna5" data-toggle="tab">Exit Permit Only (EPO)</a></li>
-                        <li><a href="#wna6" data-toggle="tab">Mutasi Data Orang Asing</a></li>
+                        <li><a href="#wna5" data-toggle="tab">Mutasi Data Orang Asing</a></li>
                     </ul>
                     
                     <div class="tab-content">
                         <div class="tab-pane active" id="wna1">
-                            Sed pretium, ligula sollicitudin laoreet viverra, tortor libero sodales leo, eget blandit nunc tortor eu nibh. Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Phasellus hendrerit. Pellentesque aliquet nibh nec urna. In nisi neque, aliquet vel, dapibus id, mattis vel, nisi. Nullam mollis.. Phasellus hendrerit. Pellentesque aliquet nibh nec urna. In nisi neque, aliquet vel, dapiPhasellus hendrerit. Pellentesque aliquet nibh nec urna. In nisi neque, aliquet vel, dapi
+                        <p> wkjkkj jkkjjkkjhjk</p>
+                            <a class="accrodian-trigger" href="#">Syarat</a>
+                            <div class="accrodian-data">
+                                <p>
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean nisl orci, condimentum ultrices consequat eu
+                                </p>
+                            </div>
+                            <a class="accrodian-trigger" href="#">Masa Berlaku</a>
+                            <div class="accrodian-data">
+                                <p>
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean nisl orci, condimentum ultrices consequat eu
+                                </p>
+                            </div>
                         </div>
                         <div class="tab-pane" id="wna2">
-                            Phasellus hendrerit. Pellentesque aliquet nibh nec urna. In nisi neque, aliquet vel, dapibus id, mattis vel, nisi. Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Sed pretium, ligula sollicitudin laoreet viverra, tortor libero sodales leo, eget blandit nunc tortor eu nibh. Nullam mollis Phasellus hendrerit. Pellentesque aliquet nibh nec urna. In nisi neque, aliquet vel, dapi
+                            <p> wkjkkj jkkjjkkjhjk</p>
+                            <a class="accrodian-trigger" href="#">Syarat</a>
+                            <div class="accrodian-data">
+                                <p>
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean nisl orci, condimentum ultrices consequat eu
+                                </p>
+                            </div>
+                            <a class="accrodian-trigger" href="#">Masa Berlaku</a>
+                            <div class="accrodian-data">
+                                <p>
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean nisl orci, condimentum ultrices consequat eu
+                                </p>
+                            </div> 
                         </div>
                         <div class="tab-pane" id="wna3">
-                            Sed pretium, ligula sollicitudin laoreet viverra, tortor libero sodales leo, eget blandit nunc tortor eu nibh. Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Phasellus hendrerit. Pellentesque aliquet nibh nec urna. In nisi neque, aliquet vel, dapibus id, mattis vel, nisi. Nullam mollis. Phasellus hendrerit. Pellentesque aliquet nibh nec urna. In nisi neque, aliquet vel, dapi
+                            <p> wkjkkj jkkjjkkjhjk</p>
+                            <a class="accrodian-trigger" href="#">Syarat</a>
+                            <div class="accrodian-data">
+                                <p>
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean nisl orci, condimentum ultrices consequat eu
+                                </p>
+                            </div>
+                            <a class="accrodian-trigger" href="#">Masa Berlaku</a>
+                            <div class="accrodian-data">
+                                <p>
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean nisl orci, condimentum ultrices consequat eu
+                                </p>
+                            </div>    
                         </div>
                         <div class="tab-pane" id="wna4">
-                            Phasellus hendrerit. Pellentesque aliquet nibh nec urna. In nisi neque, aliquet vel, dapibus id, mattis vel, nisi. Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Sed pretium, ligula sollicitudin laoreet viverra, tortor libero sodales leo, eget blandit nunc tortor eu nibh Phasellus hendrerit. Pellentesque aliquet nibh nec urna. In nisi neque, aliquet vel, dapi
+                            <p> wkjkkj jkkjjkkjhjk</p>
+                            <a class="accrodian-trigger" href="#">Syarat</a>
+                            <div class="accrodian-data">
+                                <p>
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean nisl orci, condimentum ultrices consequat eu
+                                </p>
+                            </div>
+                            <a class="accrodian-trigger" href="#">Masa Berlaku</a>
+                            <div class="accrodian-data">
+                                <p>
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean nisl orci, condimentum ultrices consequat eu
+                                </p>
+                            </div>
                         </div>
                         <div class="tab-pane" id="wna5">
-                            Sed pretium, ligula sollicitudin laoreet viverra,Phasellus hendrerit. Pellentesque aliquet nibh nec urna. In nisi neque, aliquet vel, dapibus id, mattis vel, nisi. Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Sed pretium, ligula sollicitudin laoreet viverra, tortor libero sodales leo, eget blandit nunc tortor eu nibh Phasellus hendrerit. Pellentesque aliquet nibh nec urna. In nisi neque, aliquet vel, dapi
-                        </div>
-                        <div class="tab-pane" id="wna6">
-                            kkjkljlka kjlkjkljla Sed pretium, ligula sollicitudin laoreet viverra,Phasellus hendrerit. Pellentesque aliquet nibh nec urna. In nisi neque, aliquet vel, dapibus id, mattis vel, nisi. Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Sed pretium, ligula sollicitudin laoreet viverra, tortor libero sodales leo, eget blandit nunc tortor eu nibh Phasellus hendrerit. Pellentesque aliquet nibh nec urna. In nisi neque, aliquet vel, dapi
+                            <p> wkjkkj jkkjjkkjhjk</p>
+                            <a class="accrodian-trigger" href="#">Kelahiran Anak Indonesia Ganda Terbatas</a>
+                            <div class="accrodian-data">
+                                <p>
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean nisl orci, condimentum ultrices consequat eu
+                                </p>
+                            </div>
+                            <a class="accrodian-trigger" href="#">Kelahiran Anak Warga Negara Asing</a>
+                            <div class="accrodian-data">
+                                <p>
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean nisl orci, condimentum ultrices consequat eu
+                                </p>
+                            </div>
+                            <a class="accrodian-trigger" href="#">Laporan Perkawinan</a>
+                            <div class="accrodian-data">
+                                <p>
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean nisl orci, condimentum ultrices consequat eu
+                                </p>
+                            </div>
+                            <a class="accrodian-trigger" href="#">Laporan Perceraian</a>
+                            <div class="accrodian-data">
+                                <p>
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean nisl orci, condimentum ultrices consequat eu
+                                </p>
+                            </div>
+                            <a class="accrodian-trigger" href="#">Laporan Meninggal Dunia</a>
+                            <div class="accrodian-data">
+                                <p>
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean nisl orci, condimentum ultrices consequat eu
+                                </p>
+                            </div>
+                            <a class="accrodian-trigger" href="#">Mutasi Alamat Lokal</a>
+                            <div class="accrodian-data">
+                                <p>
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean nisl orci, condimentum ultrices consequat eu
+                                </p>
+                            </div>
+                            <a class="accrodian-trigger" href="#">Mutasi Alamat dari/ ke Kantor Imigrasi Lain</a>
+                            <div class="accrodian-data">
+                                <p>
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean nisl orci, condimentum ultrices consequat eu
+                                </p>
+                            </div>
                         </div>
                     </div>
                     
