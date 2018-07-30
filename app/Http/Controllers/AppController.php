7cb16ae6c47e76f12b8719110713017362691ca9
Yusuf Dwi Santoso <yusufdwisantoso@gmail.com>
2018-07-30 08:19:04 +0700

    dikit

--- app/Http/Controllers/AppController.php
+++ app/Http/Controllers/AppController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\View;
+use App\Berita;
+
 
 class AppController extends Controller
 {
--- resources/views/app/beritaimigrasi.blade.php
+++ resources/views/app/beritaimigrasi.blade.php
@@ -1,5 +1,6 @@
 @extends('layouts.app')
 
+
 @section('content')
     <section class="span10  ">
         <div class="col-right">
