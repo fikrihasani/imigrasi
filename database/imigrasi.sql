fc5e4fd4e54d871bc0f5836d0b0dbd3c567233c9
fikrihasani <mfikrihasani@gmail.com>
2018-08-19 22:46:31 +0700

    tambahan db

--- database/imigrasi.sql
+++ database/imigrasi.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.2
+-- version 4.8.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Aug 04, 2018 at 01:43 PM
--- Server version: 10.1.34-MariaDB
--- PHP Version: 7.2.7
+-- Generation Time: Aug 19, 2018 at 05:45 PM
+-- Server version: 10.1.33-MariaDB
+-- PHP Version: 7.2.6
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -43,9 +43,8 @@ CREATE TABLE `berita` (
 --
 
 INSERT INTO `berita` (`id`, `judul_berita`, `foto_berita`, `konten_berita`, `tipe_berita`, `created_at`, `updated_at`) VALUES
-(6, 'Berita 1 Coba', 'imigrasi-mantab1_1533360809.png', 'Aku Bisa', '0', '2018-08-03 22:33:29', '2018-08-03 22:33:29'),
-(7, 'hggghghhj hhjhghjg', 'allinonepc_1533361154.jpg', 'sdyyjkjj jhkjh', '0', '2018-08-03 22:39:14', '2018-08-03 22:39:14'),
-(8, 'ahjajhaj', '246161_1533379183.jpg', 'berita baru buat dihapus', '0', '2018-08-04 03:39:43', '2018-08-04 03:39:43');
+(3, 'Berita 1', 'image b_1533196908.jpg', 'Berita 1 euy', '0', '2018-08-02 00:53:17', '2018-08-02 01:01:48'),
+(4, 'Berita 2', 'image b_1533196473.jpg', 'Ulala', '1', '2018-08-02 00:54:33', '2018-08-02 00:54:33');
 
 -- --------------------------------------------------------
 
@@ -76,24 +75,13 @@ CREATE TABLE `info_publik` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `kakanim`
+-- Table structure for table `kepala_masa`
 --
 
-CREATE TABLE `kakanim` (
-  `id_kakanim` int(10) UNSIGNED NOT NULL,
-  `nama_kakanim` varchar(255) NOT NULL,
-  `foto_kakanim` varchar(255) NOT NULL,
-  `deskripsi` text NOT NULL,
-  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  `updated_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00'
+CREATE TABLE `kepala_masa` (
+  `foto_kepala` int(20) NOT NULL,
+  `deskripsi_kepala` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `kakanim`
---
-
-INSERT INTO `kakanim` (`id_kakanim`, `nama_kakanim`, `foto_kakanim`, `deskripsi`, `created_at`, `updated_at`) VALUES
-(1, 'jhajkhjah hahahah', 'e4ccd6202b372f5938150c45db8185ce--stars_1533378657.jpg', 'jhahjkhdajh', '2018-08-04 10:30:57', '2018-08-04 03:30:57');
 
 -- --------------------------------------------------------
 
@@ -135,11 +123,25 @@ INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
 --
 
 CREATE TABLE `pengaduan` (
+  `id` int(11) NOT NULL,
   `nama` varchar(35) NOT NULL,
   `email` varchar(40) NOT NULL,
   `subjek` varchar(60) NOT NULL,
-  `pesan` text NOT NULL
+  `pesan` text NOT NULL,
+  `status` int(11) NOT NULL,
+  `foto_ktp` varchar(30) NOT NULL,
+  `kode_unik` varchar(10) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `pengaduan`
+--
+
+INSERT INTO `pengaduan` (`id`, `nama`, `email`, `subjek`, `pesan`, `status`, `foto_ktp`, `kode_unik`, `created_at`, `updated_at`) VALUES
+(1, 'Fikri Hasani', 'mfikrihasani@gmail.com', 'nganu mas', 'ya gitu ya', -1, 'image b_1534232556.jpg', 'png5260', '2018-08-14 00:42:36', '2018-08-14 00:42:36'),
+(2, 'fikyun', 'fikyun@gmail.com', 'gini mas', 'gitu deh', -1, 'image a_1534237120.jpg', 'png4229', '2018-08-14 01:58:40', '2018-08-14 01:58:40');
 
 -- --------------------------------------------------------
 
@@ -148,8 +150,7 @@ CREATE TABLE `pengaduan` (
 --
 
 CREATE TABLE `profil_pejabat` (
-  `id_pejabat` int(20) NOT NULL,
-  `foto_pejabat` varchar(255) NOT NULL,
+  `foto_pejabat` int(20) NOT NULL,
   `deskripsi_pejabat` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
@@ -207,8 +208,7 @@ CREATE TABLE `users` (
 
 INSERT INTO `users` (`id`, `name`, `email`, `password`, `remember_token`, `created_at`, `updated_at`) VALUES
 (4, 'yusuf', 'yusufdwisantoso@gmail.com', '$2y$10$IybvBgAvQT60lEZTadHzWeh87rYGTE9yT1t5f69.YwkXuRj7KcCYa', 'MXIDRsNRIY5ESOSoYqs8DOdime9JYVLb1hYGIpq41YOlw11v1PAgLeiS83sW', '2018-07-13 09:45:12', '2018-07-13 09:45:12'),
-(5, 'fikri', 'mfikrihasani@gmail.com', '$2y$10$vzSIpKDIAP0J92yqq8EqxO1MhWKCdCxSZPe.r/bkJlXHMpDvCNNS.', 'eloliifpOIu9XSqOxNI07jWoSFTYUCIJ3Y1YXwkPtwCHinCXLtQh260ihO5P', '2018-07-16 01:31:36', '2018-07-16 01:31:36'),
-(6, 'santoso', 'kulitinta13@gmail.com', '$2y$10$ewqlLjenjQ8gCv.Ufe.65OyheX5B6cE7Qn8/5FKXA9g/6jiG1MgmG', NULL, '2018-08-03 22:00:44', '2018-08-03 22:00:44');
+(5, 'fikri', 'mfikrihasani@gmail.com', '$2y$10$vzSIpKDIAP0J92yqq8EqxO1MhWKCdCxSZPe.r/bkJlXHMpDvCNNS.', 'eloliifpOIu9XSqOxNI07jWoSFTYUCIJ3Y1YXwkPtwCHinCXLtQh260ihO5P', '2018-07-16 01:31:36', '2018-07-16 01:31:36');
 
 -- --------------------------------------------------------
 
@@ -234,22 +234,16 @@ ALTER TABLE `berita`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `kakanim`
---
-ALTER TABLE `kakanim`
-  ADD PRIMARY KEY (`id_kakanim`);
-
---
 -- Indexes for table `migrations`
 --
 ALTER TABLE `migrations`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `profil_pejabat`
+-- Indexes for table `pengaduan`
 --
-ALTER TABLE `profil_pejabat`
-  ADD PRIMARY KEY (`id_pejabat`);
+ALTER TABLE `pengaduan`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `users`
@@ -265,13 +259,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `berita`
 --
 ALTER TABLE `berita`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
-
---
--- AUTO_INCREMENT for table `kakanim`
---
-ALTER TABLE `kakanim`
-  MODIFY `id_kakanim` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `migrations`
@@ -280,10 +268,16 @@ ALTER TABLE `migrations`
   MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
+-- AUTO_INCREMENT for table `pengaduan`
+--
+ALTER TABLE `pengaduan`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
